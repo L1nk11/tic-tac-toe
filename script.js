276b22f4d8a7360824555c9d1adb3a0ce169937c
Julio Oliveira <julio.oliveira06@outlook.com>
2025-02-27 13:16:58 -0300

    added position mark logic and player objects

--- script.js
+++ script.js
@@ -5,16 +5,24 @@ const start = document.getElementById('start')
 
 let currentPlayerTurn = undefined
 
-// output either 1 or 0 | 1 = X | 0 = O
-function defineFirstPlayer() {
-    return Math.round(Math.random())
+let boardList = []
+
+function createPlayer(symbol) {
+    let score = 0
+
+    const increaseScore = () => score += 1
+    const resetScore = () => score = 0
+
+    return {symbol, increaseScore, resetScore}
 }
 
 function startMessage() {
     const background = document.createElement('div')
     const backgroundTop = document.createElement('div')
     const backgroundBottom = document.createElement('div')
-    let firstPlayer = defineFirstPlayer()
+
+    // output either 1 or 0 | 1 = X | 0 = O
+    let firstPlayer = Math.round(Math.random())
 
     // clean the board and set the style
     board.innerHTML = ''
@@ -29,8 +37,10 @@ function startMessage() {
 
     if (firstPlayer == 1) {
         backgroundBottom.textContent = 'X'
+        currentPlayerTurn = player1
     } else {
         backgroundBottom.textContent = 'O'
+        currentPlayerTurn = player2
     }
 
     background.addEventListener('click', () => {
@@ -41,7 +51,6 @@ function startMessage() {
         generateBoard()
     });
 
-    
     background.appendChild(backgroundTop)
     background.appendChild(backgroundBottom)
     
@@ -51,7 +60,6 @@ function startMessage() {
 
 function generateBoard() {
     board.innerHTML = ''
-
     board.style.gridTemplateColumns = 'repeat(3, 1fr)'
     board.style.gridTemplateRows = 'repeat(3, 1fr)'
     
@@ -61,8 +69,35 @@ function generateBoard() {
     // positions has been matched, if yes the position 
     // matched should win
 
-    
-    
+    for (let index = 0; index < 9; index++) {
+
+        const cell = document.createElement('div')
+        cell.classList.add('cell')
+
+        let isCellMarked = false
+
+        cell.addEventListener('click', function(){
+            // see who turn it is, place the simbol in the div
+            // see if there is a winner, if yes stop game 
+            // and declare it, if not change turn and continue
+
+
+            if (currentPlayerTurn == player1 && isCellMarked == false) {
+                console.log('player1 turn')
+                cell.textContent = player1.symbol
+                isCellMarked = true
+                currentPlayerTurn = player2
+            } else if (currentPlayerTurn == player2 && isCellMarked == false) {
+                console.log('player2 turn')
+                cell.textContent = player2.symbol
+                isCellMarked = true
+                currentPlayerTurn = player1
+            }
+
+        })
+
+        board.appendChild(cell)
+    }
 }
 
 start.addEventListener('click', () => {
@@ -77,17 +112,30 @@ reset.addEventListener('click', function() {
     
 })
 
+function populateList() {
+    
+}
+
 function restartBoard() {
     board.innerHTML = ''
+    board.style.gridTemplateColumns = '1fr'
+    board.style.gridTemplateRows = '1fr'
 
     const startDiv = document.createElement('div')
     startDiv.classList.add('start')
     startDiv.textContent = '>Start Game<'
     startDiv.id = 'start'
 
+    player1.resetScore()
+    player2.resetScore()
+
     startDiv.addEventListener('click', function() {
+
         startMessage()
     })
 
     board.appendChild(startDiv)
 }
+
+const player1 = createPlayer('X')
+const player2 = createPlayer('O')
